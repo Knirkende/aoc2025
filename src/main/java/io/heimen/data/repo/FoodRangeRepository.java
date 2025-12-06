5e469c971f25267ce89acd2f3cf6680ded50ec15
Ole Holgernes <ole.holgernes@gmail.com>
2025-12-06 13:49:23 +0100

    solve day 5 part 2

--- src/main/java/io/heimen/data/repo/FoodRangeRepository.java
+++ src/main/java/io/heimen/data/repo/FoodRangeRepository.java
@@ -7,11 +7,12 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-//@Repository
-//@Component
 public interface FoodRangeRepository extends CrudRepository<FoodRange, Long> {
 
     @Query(value = "SELECT f FROM FoodRange f WHERE :item BETWEEN f.rangelow AND f.rangehigh")
     List<FoodRange> findRanges(@Param("item") long item);
+
+    @Query(value = "SELECT f FROM FoodRange f ORDER BY f.rangelow ASC")
+    List<FoodRange> findAllRangesSortedByRangeLow();
 
 }
--- src/main/java/io/heimen/solver/Day5Solver.java
+++ src/main/java/io/heimen/solver/Day5Solver.java
@@ -1,11 +1,12 @@
 package io.heimen.solver;
 
+import io.heimen.data.model.FoodRange;
 import io.heimen.data.repo.FoodRangeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -30,28 +31,25 @@ public class Day5Solver {
     }
 
     public long solvePartTwo() {
-        //sort intervals by start
-        //create a list of final intervals
-        //for each interval, compare to previous:
-        //  start less than or equal to end of previous? overlap. Start of final interval will be start of previous
-        //    then: end of final interval to max of the two ends
-        //    then: compare next interval to last interval in final list
         var res = 0L;
-        var allRanges = foodRangeRepo.findAll();
-        var goodFood = new HashSet<Long>();
-        var counter = 0;
-        for (var range : allRanges) {
-            counter++;
-            logger.info("Working on range {}", counter);
-            for (long i = range.getRangelow(); i <= range.getRangehigh(); i++) {
-                goodFood.add(i);
-                if (goodFood.size() >= 10000) {
-                    res += 10000;
-                    goodFood.clear();
-                }
+        List<FoodRange> reducedRanges = new ArrayList<>();
+        var allRanges = foodRangeRepo.findAllRangesSortedByRangeLow();
+        reducedRanges.add(allRanges.getFirst());
+        var iterator = allRanges.listIterator(1);
+        while (iterator.hasNext()) {
+            var current = iterator.next();
+            if (current.getRangelow() <= reducedRanges.getLast().getRangehigh()) {
+                var previous = reducedRanges.removeLast();
+                var combinedRange = new FoodRange(previous.getRangelow(), Math.max(previous.getRangehigh(), current.getRangehigh()));
+                reducedRanges.add(combinedRange);
+            } else {
+                reducedRanges.add(current);
             }
         }
-        res += goodFood.size();
+        for (FoodRange range : reducedRanges) {
+            res += range.getRangehigh() + 1 - range.getRangelow();
+        }
+
         return res;
     }
 }
